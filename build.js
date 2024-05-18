b3f8d23c46e368f4ac2a6ad235ec510e0d5f89cf
agracio <antonas.graciovas@gmail.com>
2024-05-18 19:26:38 +0100

    updating build script

--- build.js
+++ build.js
@@ -14,40 +14,11 @@ spawn('dotnet', ['restore', 'EdgeFs.sln'], { stdio: 'inherit', cwd: path.resolve
         }
     });
 
-// run('dotnet', ['restore', 'EdgeFs.sln'], function(code, signal) {
-//     if (code === 0) {
-//         run('dotnet', ['build', 'EdgeFs.sln'], function(code, signal) {
-//             if (code === 0) {runOnSuccess}
-//         });
-//     }
-// });
-
-// function run(cmd, args, onClose){
-
-// 	var params = process.env.EDGE_USE_CORECLR ? {cwd: testDir} : {};
-//     var command = spawn(cmd, args, params);
-//     var result = '';
-//     var error = '';
-//     command.stdout.on('data', function(data) {
-//         result += data.toString();
-//     });
-//     command.stderr.on('data', function(data) {
-//         error += data.toString();
-//     });
-
-//     command.on('error', function(err) {
-//         console.log(error);
-//         console.log(err);
-//     });
-
-//     command.on('close', function(code){
-//         onClose(code, '');
-// 	});
-// }
 function copy() {
 
     fs.copyFileSync('src/edge-fs/bin/Release/edge-fs.dll', 'lib/edge-fs.dll')
     fs.copyFileSync('src/edge-fs-coreclr/bin/Release/edge-fs-coreclr.dll', 'lib/edge-fs-coreclr.dll')
+    fs.copyFileSync('src/edge-fs-coreclr/bin/Release/edge-fs-coreclr.deps.json', 'lib/edge-fs-coreclr.deps.json')
 
 }
 
